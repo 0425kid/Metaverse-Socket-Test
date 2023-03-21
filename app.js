1f89ec6a4a26bb143fb94c264354019ec86122d9
0425kid <0425kid@gmail.com>
2023-03-21 19:37:29 +0900

    포트 재변경

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ io.on('connection', function(socket){
   })
 })
 
-server.listen(8080, function() {
+server.listen(3000, function() {
   console.log('Socket IO server listening on port 3000');
 });
