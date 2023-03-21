aebdeba80718d905f6eb1b9cf7d89ba332fad3a4
0425kid <0425kid@gmail.com>
2023-03-21 19:38:19 +0900

    포트 3001

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ io.on('connection', function(socket){
   })
 })
 
-server.listen(3000, function() {
-  console.log('Socket IO server listening on port 3000');
+server.listen(3001, function() {
+  console.log('Socket IO server listening on port 3001');
 });
